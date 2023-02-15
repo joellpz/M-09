1ad529d692a6f04f6c2b0710d9ed3403c4214d5f
joellpz <joel@joel.com>
2023-02-15 17:23:44 +0100

    UDP

---
+++ UF3/SocketsAndServices/URL/ServerClientUDP/DatagramSocketClient.java
@@ -0,0 +1,76 @@
+package UF3.SocketsAndServices.URL.ServerClientUDP;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.Scanner;
+
+public class DatagramSocketClient {
+    InetAddress serverIP;
+    int serverPort;
+    DatagramSocket socket;
+    Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        DatagramSocketClient datagramSocketClient = new DatagramSocketClient();
+        try {
+            datagramSocketClient.init("192.168.22.112",5555);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void init(String host, int port) throws IOException {
+        serverIP = InetAddress.getByName(host);
+        serverPort = port;
+        socket = new DatagramSocket();
+        runClient();
+    }
+
+    public void runClient() throws IOException {
+        byte [] receivedData = new byte[1024];
+        byte [] sendingData;
+
+        //a l'inici
+        sendingData = getFirstRequest();
+        //el servidor atén el port indefinidament
+        while(mustContinue(sendingData)){
+            DatagramPacket packet = new DatagramPacket(sendingData,
+                    sendingData.length,
+                    serverIP,
+                    serverPort);
+            //enviament de la resposta
+            socket.send(packet);
+
+            //creació del paquet per rebre les dades
+            packet = new DatagramPacket(receivedData, 1024);
+            //espera de les dades
+            socket.receive(packet);
+            //processament de les dades rebudes i obtenció de la resposta
+            sendingData = getDataToRequest(packet.getData(), packet.getLength());
+        }
+
+        System.out.println("Tancant....");
+    }
+
+    private byte[] getDataToRequest(byte[] data, int length) {
+        //procés diferent per cada aplicació
+        String msg = new String(data,0,length);
+        System.out.println("Respuesta:" + msg);
+        System.out.println("Mensaje a enviar:");
+        return sc.nextLine().getBytes();
+    }
+
+    private byte[] getFirstRequest() {
+        //procés diferent per cada aplicació
+        System.out.println("Asigname Tu NOMBRE parguela!");
+        return sc.nextLine().getBytes();
+
+    }
+
+
+    private boolean mustContinue(byte[] sendingData) {
+        //procés diferent per cada aplicació
+        if (new String(sendingData,0,sendingData.length).equals("adeu")) return false;
+        else return true;
+    }
+}
---
+++ UF3/SocketsAndServices/URL/ServerClientUDP/DatagramSocketServer.java
@@ -0,0 +1,59 @@
+package UF3.SocketsAndServices.URL.ServerClientUDP;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+public class DatagramSocketServer {
+
+    DatagramSocket socket;
+    public static void main(String[] args) {
+        DatagramSocketServer datagramSocketServer = new DatagramSocketServer();
+        try {
+            datagramSocketServer.init(2735);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public void init(int port) throws IOException {
+        socket = new DatagramSocket(port);
+        runServer();
+    }
+
+    public void runServer() throws IOException {
+        byte[] receivingData = new byte[1024];
+        byte[] sendingData;
+        InetAddress clientIP;
+        int clientPort;
+
+        //el servidor atén el port indefinidament
+        while (true) {
+            //creació del paquet per rebre les dades
+            DatagramPacket packet = new DatagramPacket(receivingData, 1024);
+            //espera de les dades
+            socket.receive(packet);
+            //processament de les dades rebudes i obtenció de la resposta
+            sendingData = processData(packet.getData(), packet.getLength());
+            //obtenció de l'adreça del client
+            clientIP = packet.getAddress();
+            //obtenció del port del client
+            clientPort = packet.getPort();
+            //creació del paquet per enviar la resposta
+            packet = new DatagramPacket(sendingData, sendingData.length,
+                    clientIP, clientPort);
+            //enviament de la resposta
+            socket.send(packet);
+        }
+    }
+
+    private byte[] processData(byte[] data, int length) {
+        //procés diferent per cada aplicació
+        String msg =new String(data,0,length);
+        System.out.println(msg);
+        return msg.toUpperCase().getBytes();
+    }
+}
